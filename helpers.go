a39a70c878f90dec9864d96c707c5853df73c47a
Hichem KHIAL <khial.ali.hichem@gmail.com>
2019-12-04 07:49:22 -0500

    add slice shuffle function

--- helpers.go
+++ helpers.go
@@ -1,8 +1,22 @@
 package helpers
 
+import (
+	"math/rand"
+	"time"
+)
+
 func Swap(left *int, right *int) {
 	tmp := *left
 	*left = *right
 	*right = tmp
 }
 
+func Shuffle(arr []int) {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for len(arr) > 0 {
+		n := len(arr)
+		randIndex := r.Intn(n)
+		arr[n-1], arr[randIndex] = arr[randIndex], arr[n-1]
+		arr = arr[:n-1]
+	}
+}
